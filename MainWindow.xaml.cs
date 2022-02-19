f197a10ee6b30697a8abc5d0b07d2742ef2c094f
krupovic <petrkruppa@gmail.com>
2022-02-20 01:28:11 +0300

    Minor bugfix / Copyright added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using static System.Math;  
+using static System.Math;
+using System.Diagnostics;
 
 namespace Calculator
 {
@@ -24,6 +25,7 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+            Copyright.Text = "Copyright 2022 Petr Kruppa, Kuzma Skorkin\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
         }
 
         private void ButtonClicked (object sender, RoutedEventArgs e)
@@ -97,6 +99,16 @@ namespace Calculator
             }
 
             return DecToM(NToDec(x, n), m);
+
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
         }
     }
